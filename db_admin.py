0089d5858042fe78c12c7198b234a75feff2223f
dk <hello.dk@outlook.com>
2015-05-06 14:37:49 +0530

    removed errors

--- db_admin.py
+++ db_admin.py
@@ -6,13 +6,16 @@ def get_db_connection():
 	r.connect(config.RETHINK_DB_SERVER, config.PORT_FOR_CLIENT_DRIVERS).repl()
 
 def create_db(db_name):
+	pass
 
 def create_table(table_name):
 	r.db('dk').table_create('tv_shows').run()
 	r.table('tv_shows').insert({ 'name': 'Star Trek TNG' }).run()
 
 def delete_table(table_name):
+	pass
 
 def delete_db(db_name):
+	pass
 
 
