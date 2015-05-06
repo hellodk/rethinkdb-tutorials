cfbd8155fc9180663dd82739a52b2942432225be
dk <hello.dk@outlook.com>
2015-05-06 14:36:18 +0530

    Added functions for creating a table

--- db_admin.py
+++ db_admin.py
@@ -2,13 +2,17 @@ import config
 import rethinkdb as r
 
 
-def connect_db():
-	r.connect('localhost', 28015).repl()
-	r.db('test').table_create('tv_shows').run()
-	r.table('tv_shows').insert({ 'name': 'Star Trek TNG' }).run()
-
+def get_db_connection():
+	r.connect(config.RETHINK_DB_SERVER, config.PORT_FOR_CLIENT_DRIVERS).repl()
 
 def create_db(db_name):
-	
 
 def create_table(table_name):
+	r.db('dk').table_create('tv_shows').run()
+	r.table('tv_shows').insert({ 'name': 'Star Trek TNG' }).run()
+
+def delete_table(table_name):
+
+def delete_db(db_name):
+
+
