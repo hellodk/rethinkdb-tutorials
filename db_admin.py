551bbe49f29462dd202cb7ac3c3a6888727324dd
hellodk <deepak.gupta@knowlarity.com>
2015-05-11 13:07:29 +0530

    changes

--- db_admin.py
+++ db_admin.py
@@ -4,63 +4,67 @@ import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError
 
 def get_db_connection():
-	try:
-		#conn = r.connect(host='localhost', port=28015)
-		global connection
-		connection = r.connect(config.RETHINK_DB_INSTANCE_HOST, config.DRIVER_PORT).repl()
-		print 'db connection created ',connection
-	except Exception as ex:
-		print 'Connection Establishment faield ', ex.message()
-	'''	r.connect({ host: 'localhost',
-            port: 28015,
-            db: 'marvel',
-            authKey: 'hunter2' },
-          function(err, conn) { ... })'''
-	return connection
+    try:
+        #conn = r.connect(host='localhost', port=28015)
+        global connection
+        connection = r.connect(config.RETHINK_DB_INSTANCE_HOST, config.DRIVER_PORT).repl()
+        print 'db connection created ',connection
+    except Exception as ex:
+        print 'Connection Establishment faield ', ex.message()
+    # '''    r.connect({ host: 'localhost',
+    #         port: 28015,
+    #         db: 'marvel',
+    #         authKey: 'hunter2' },
+    #       function(err, conn) { ... })'''
+    return connection
 
 def close_connection():
-	pass
+    try:
+        connection.close()
+        print 'connection closed'
+    except Exception as ex:
+        print 'Issue while closing the connection ',ex
 
 def create_db(db_name):
-	try:
-		r.db_create(db_name).run()
-	except RqlRuntimeError as rql:
-		print 'Failed to create the database ', rql
-	except Exception as ex:
-		print 'Exception occurred ' , ex
+    try:
+        r.db_create(db_name).run()
+    except RqlRuntimeError as rql:
+        print 'Failed to create the database ', rql
+    except Exception as ex:
+        print 'Exception occurred ' , ex
 
 def list_db():
-	#get_db_connection()
-	print 'value ', connection
-	r.db_list().run(connection)
+    #get_db_connection()
+    print r.db_list().run(connection)
 
 def create_table(db_name,table_name):
-	try:
-		r.db(db_name).table_create(table_name).run()
-		print 'Table created'
-	except:
-		print 'Caught in exception'
+    try:
+        r.db(db_name).table_create(table_name).run()
+        print 'Table created'
+    except:
+        print 'Caught in exception'
         r.table(table_name).insert(r.expr({'age': 26, 'name': 'Michel'}))
-		
-		#r.table(table_name).insert({'age': 26, 'name': 'Michel'}).run()
+        
+        #r.table(table_name).insert({'age': 26, 'name': 'Michel'}).run()
 
-	print 'Exception in writing to table'
+    print 'Exception in writing to table'
 
 def delete_table(table_name):
-	pass
+    pass
 
 def delete_db(db_name):
-	pass
+    pass
 
 if __name__ == '__main__':
-	print 'hello'
+    print 'hello'
 
-	get_db_connection()
-	print 'db connection established'
-	create_db(config.DATABASE)
-	#print config.DATABASE, ' db created'
-	print 'Listing existing database \n', list_db()
-	#create_table('dk','phn')
+    get_db_connection()
+    print 'db connection established'
+    create_db(config.DATABASE)
+    #print config.DATABASE, ' db created'
+    print 'Listing existing database \n', list_db()
+    #create_table('dk','phn')
+    close_connection()
 
 '''
 r.table('dk.phn').insert({"age": 22,"name": "Miachel"}).run()
